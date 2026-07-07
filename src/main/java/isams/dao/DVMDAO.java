7ae927fde993ff59409c23cbc91429ac1baa0df2
Fitri Mifzal <2024800492@student.uitm.edu.my>
2026-07-07 23:20:55 +0800

    Fix STUD_ID to STU_ID in SVMDAO and DVMDAO

--- src/main/java/isams/dao/DVMDAO.java
+++ src/main/java/isams/dao/DVMDAO.java
@@ -1,36 +1,29 @@
 package isams.dao;
-
 import java.sql.*;
 import isams.connection.ConnectionManager;
 import isams.model.DVM;
-
 public class DVMDAO {
-
     public static void upsertDVM(DVM dvm) {
         try {
             Connection con = ConnectionManager.getConnection();
-
-            String checkSql = "SELECT 1 FROM DVM WHERE STUD_ID=?";
+            String checkSql = "SELECT 1 FROM DVM WHERE STU_ID=?";
             PreparedStatement check = con.prepareStatement(checkSql);
             check.setInt(1, dvm.getStuId());
             ResultSet rs = check.executeQuery();
-
             if (rs.next()) {
-                String sql = "UPDATE DVM SET REPEATPAPER=? WHERE STUD_ID=?";
+                String sql = "UPDATE DVM SET REPEATPAPER=? WHERE STU_ID=?";
                 PreparedStatement ps = con.prepareStatement(sql);
                 ps.setInt(1, dvm.getRepeatPaper());
                 ps.setInt(2, dvm.getStuId());
                 ps.executeUpdate();
             } else {
-                String sql = "INSERT INTO DVM (STUD_ID, REPEATPAPER) VALUES (?, ?)";
+                String sql = "INSERT INTO DVM (STU_ID, REPEATPAPER) VALUES (?, ?)";
                 PreparedStatement ps = con.prepareStatement(sql);
                 ps.setInt(1, dvm.getStuId());
                 ps.setInt(2, dvm.getRepeatPaper());
                 ps.executeUpdate();
             }
-
             con.close();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
